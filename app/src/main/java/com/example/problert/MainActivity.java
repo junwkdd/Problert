1af3049e0fcdbc55df0a206699f71f2c789343ba
junwkdd <junwkdd@gmail.com>
2019-08-28 23:53:17 +0900

    Initial commit

---
+++ app/src/main/java/com/example/problert/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.problert;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
