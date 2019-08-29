11909ffc527cdcb806f7715d5337a6d68c82614b
junwkdd <junwkdd@gmail.com>
2019-08-29 13:04:32 +0900

    W

--- app/src/main/java/com/example/problert/MainActivity.java
+++ app/src/main/java/com/example/problert/MainActivity.java
@@ -1,8 +1,20 @@
 package com.example.problert;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +22,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(RetrofitService.URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        final RetrofitService retrofitExService = retrofit.create(RetrofitService.class);
+        Button button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                HashMap<String, Object> input = new HashMap<>();
+                input.put("userId", 1);
+                input.put("title", "title");
+                input.put("description", "description");
+
+                retrofitExService.postData(input).enqueue(new Callback<Data>() {
+                    @Override
+                    public void onResponse(@NonNull Call<Data> call, @NonNull Response<Data> response) {
+                        if (response.isSuccessful()) {
+                            Data body = response.body();
+                            if (body != null) {
+                                Log.d("data.getTitle()", body.getTitle()+"");
+                                Log.d("data.getDescription()", body.getDescription()+"");
+                                Log.e("postData end", "======================================");
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(@NonNull Call<Data> call, @NonNull Throwable t) {
+                        Log.e("postData failed", "======================================");
+                    }
+                });
+            }
+        });
     }
 }
