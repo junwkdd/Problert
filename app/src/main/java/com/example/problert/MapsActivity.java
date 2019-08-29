03f7cd55d8494e213839850fb0f5c9fd71d6799f
junwkdd <junwkdd@gmail.com>
2019-08-30 00:59:18 +0900

    W

--- app/src/main/java/com/example/problert/MapsActivity.java
+++ app/src/main/java/com/example/problert/MapsActivity.java
@@ -6,7 +6,6 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -14,11 +13,6 @@ import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
@@ -33,36 +27,24 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.gson.Gson;
 //lat 위도 lag 경도
-
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
     private static final int REQUEST_CODE_PERMISSIONS = 1000;
     private FusedLocationProviderClient mFusedLocationClient;
     private GoogleMap mMap;
-    Marker selectedMarker;
-    private double lat;
-    private double lng;
-    Retrofit retrofit = new Retrofit.Builder()
-            .baseUrl(RetrofitService.URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build();
-    final RetrofitService retrofitService = retrofit.create(RetrofitService.class);
+
 
     public Marker addmarking(){
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, REQUEST_CODE_PERMISSIONS);
             return null;
         }
-
         mFusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
             @Override
             public void onSuccess(Location location) {
                 if (location != null) {
                     // 현재 위치
-                    lat = location.getLatitude();
-                    lng = location.getLongitude();
-                    Double.toString(lat);
-                    Double.toString(lng);
                     LatLng myLocation = new LatLng(location.getLatitude(), location.getLongitude());
+<<<<<<< HEAD
 
                     retrofitService.getData(lat+"", lng+"").enqueue(new Callback<Data>() {
                         @Override
@@ -91,6 +73,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     });
 
 
+=======
+>>>>>>> 9b4cdf930db1b0122803e5cf9c030fe97f62f648
                     mMap.addMarker(new MarkerOptions()
                             .position(myLocation)
                             .title("내 위치")
@@ -105,21 +89,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         return null;
     }
 
-    public void changeSelectedMarker(Marker marker) {
-        if (selectedMarker != null){
-            addmarking();
-            selectedMarker.remove();
-        }
-
-        if (marker != null) {
-            selectedMarker = addmarking();
-            marker.remove();
-        }
-    }
-
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+
+        LatLng seoul = new LatLng(35.14291, 126.799890);
+        mMap.addMarker(new MarkerOptions().position(seoul).title("내 위치"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(seoul));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(17.0f));
     }
 
     @RequiresApi(api = Build.VERSION_CODES.M)
@@ -132,7 +109,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
 
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+
         addmarking();
+
     }
 
     /**
@@ -153,7 +133,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         switch (requestCode) {
             case REQUEST_CODE_PERMISSIONS:
                 if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                        Toast.makeText(this, "권한 체크 거부 됨", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(this, "권한 체크 거부 됨", Toast.LENGTH_SHORT).show();
                 }
                 return;
         }
@@ -162,6 +142,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     @RequiresApi(api = Build.VERSION_CODES.M)
     public void onLastLocationButtonClicked(View view) {
+        mMap.clear();
         addmarking();
     }
 
@@ -173,8 +154,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     public void writebutton(View view){
         Intent intentw = new Intent(this, MainActivity.class);
-        intentw.putExtra("lat", lat);
-        intentw.putExtra("lng", lng);
         startActivity(intentw);
     }
 }
