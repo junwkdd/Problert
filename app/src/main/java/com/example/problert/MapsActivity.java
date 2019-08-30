f6c30b1e80d02050a3887e7570b5e973641d417d
YangHyunSeung <41092020+zmfoq12345@users.noreply.github.com>
2019-08-30 23:31:08 +0900

    Update MapsActivity.java

--- app/src/main/java/com/example/problert/MapsActivity.java
+++ app/src/main/java/com/example/problert/MapsActivity.java
@@ -3,6 +3,8 @@ package com.example.problert;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.os.Build;
 import android.os.Bundle;
@@ -26,9 +28,10 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.gson.Gson;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.Locale;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -37,11 +40,12 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 //lat 위도 lag 경도
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
     private static final int REQUEST_CODE_PERMISSIONS = 1000;
     private FusedLocationProviderClient mFusedLocationClient;
     private GoogleMap mMap;
-
+    private List<Address> address;
+    private String ad;
     private double lat;
     private double lng;
     double locations[][];
@@ -51,7 +55,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             .build();
     final RetrofitService retrofitService = retrofit.create(RetrofitService.class);
 
-    public Marker addmarking(){
+    public Marker addmarking() {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, REQUEST_CODE_PERMISSIONS);
             return null;
@@ -67,6 +71,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     Double.toString(lat);
                     Double.toString(lng);
                     LatLng myLocation = new LatLng(location.getLatitude(), location.getLongitude());
+
+                    try {
+                        ad = findAddress(lat, lng);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
 
                     retrofitService.getData(lat+"", lng+"").enqueue(new Callback<List<Data>>() {
                         @Override
@@ -97,7 +107,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                             .position(myLocation)
                             .title("내 위치")
                             .icon(BitmapDescriptorFactory.fromResource(R.drawable.redpin))
-                            .snippet("여의도 한강 치맥 합시다."));
+                            .snippet(ad));
                     mMap.moveCamera(CameraUpdateFactory.newLatLng(myLocation));
                     // 카메라 줌
                     mMap.animateCamera(CameraUpdateFactory.zoomTo(17.0f));
@@ -110,6 +120,18 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+
+//      mMap.addMarker(new MarkerOptions().position(new LatLng(35.1426, 126.8)));
+        mMap.setOnMarkerClickListener((GoogleMap.OnMarkerClickListener) this);
+        addmarking();
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker){
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(marker.getPosition()));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(17.0f));
+        Toast.makeText(this, marker.getTitle()+"\n"+marker.getSnippet(), Toast.LENGTH_SHORT).show();
+        return true;
     }
 
     @RequiresApi(api = Build.VERSION_CODES.M)
@@ -117,12 +139,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment   = (SupportMapFragment) getSupportFragmentManager()
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-        addmarking();
     }
 
     /**
@@ -151,20 +172,29 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
     @RequiresApi(api = Build.VERSION_CODES.M)
-    public void onLastLocationButtonClicked(View view) {
+    public void onLastLocationButtonClicked(View view) throws IOException {
+        mMap.clear();
         addmarking();
     }
 
-//    public  void address(){
-//        Context mConText = null;
-//        Geocoder mGeocoder = new Geocoder(mConText);
-//
-//    }
-
-    public void writebutton(View view){
+    public void writebutton(View view) {
         Intent intentw = new Intent(this, MainActivity.class);
         intentw.putExtra("lat", lat);
         intentw.putExtra("lng", lng);
         startActivity(intentw);
+    }
+
+    private String findAddress(double a, double b) throws IOException {
+        StringBuffer bf = new StringBuffer();
+        Geocoder geocoder = new Geocoder(this, Locale.KOREA);
+        address = geocoder.getFromLocation(a, b, 1);
+        Log.d("address", address+"");
+        if (address != null && address.size() > 0) {
+            // 주소
+            String currentLocationAddress = address.get(0).getAddressLine(0).toString();
+            // 전송할 주소 데이터 (위도/경도 포함 편집)
+            bf.append(currentLocationAddress);
+        }
+        return bf.toString();
     }
 }
