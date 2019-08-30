5324189e0475b7cf3986368d707ee462368b5316
YangHyunSeung <41092020+zmfoq12345@users.noreply.github.com>
2019-08-30 18:13:14 +0900

    1app

--- app/src/main/java/com/example/problert/MapsActivity.java
+++ app/src/main/java/com/example/problert/MapsActivity.java
@@ -102,6 +102,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+
+        addmarking();
     }
 
     @RequiresApi(api = Build.VERSION_CODES.M)
